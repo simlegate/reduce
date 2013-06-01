a423d49122a6ac4f45124405943ba66420dd332e
simlegate <simlegate@163.com>
2013-06-01 23:39:22 +0800

    add app perssions to access Wifi Manager

--- reduce/src/main/java/com/simlegate/reduce/ScreenActionReceiver.java
+++ reduce/src/main/java/com/simlegate/reduce/ScreenActionReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
+import android.net.wifi.WifiManager;
 
 /**
  * Created by simlegate on 13-6-1.
@@ -17,17 +18,25 @@ public class ScreenActionReceiver extends BroadcastReceiver {
 
 
         String action = intent.getAction();
-
+        WifiManager wifiManager =  (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 
         if(Intent.ACTION_SCREEN_ON.equals(action))
         {
             Log.d(TAG, "screen is on...");
+
+            if (!wifiManager.isWifiEnabled()) {
+                wifiManager.setWifiEnabled(true);
+            }
 
         }
 
         else if(Intent.ACTION_SCREEN_OFF.equals(action))
         {
             Log.d(TAG, "screen is off...");
+
+            if (wifiManager.isWifiEnabled()) {
+                wifiManager.setWifiEnabled(false);
+            }
         }
 
         else if(Intent.ACTION_USER_PRESENT.equals(action))
